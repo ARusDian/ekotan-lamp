67619d60c65d88bbafb9dca389b2f4704cb4f79f
RUVNE <dian.galleon@gmail.com>
2023-11-18 14:43:04 +0800

    feat: api kecamatan kukar

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\StreetLightController;
+use App\Models\Kecamatan;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,6 +18,14 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('kecamatan', function (Request $request)
+{
+    return response()->json([
+        'status' => 'success',
+        'data' => Kecamatan::all()
+        ]);
 });
 
 Route::get('street-light', [StreetLightController::class,'getStreetLightsApi'])->name('api.street-light');
